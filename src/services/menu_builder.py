86f1f8683fe3dcbe8690dd68c1fb664f66b7293f
muller <pedromullergl11@gmail.com>
2023-10-05 19:00:22 -0300

    feat: menu builder implemented

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,21 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        menu = []
+
+        for dish in self.menu_data.dishes:
+            if restriction in dish.get_restrictions():
+                continue
+            elegible = all(
+                ingredient in self.inventory.inventory
+                for ingredient in dish.get_ingredients()
+            )
+            if elegible:
+                menu_item = {
+                    "dish_name": dish.name,
+                    "ingredients": dish.get_ingredients(),
+                    "price": dish.price,
+                    "restrictions": dish.get_restrictions()
+                }
+                menu.append(menu_item)
+        return menu
