4d19157c43f34b5ba1ad3bd99fe2b54723e31acf
muller <pedromullergl11@gmail.com>
2023-10-05 15:43:43 -0300

    test: ingredient tests implemented

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,17 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("Tomato")
+    ingredient2 = Ingredient("Onion")
+
+    assert ingredient1.name == "Tomato"
+
+    assert hash(ingredient1) == hash("Tomato")
+
+    assert repr(ingredient1) == "Ingredient('Tomato')"
+
+    assert ingredient1 == ingredient1
+
+    assert ingredient1 != ingredient2
+
+    assert ingredient1.restrictions == set()
